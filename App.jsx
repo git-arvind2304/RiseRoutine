e3e2fe7c3d2c7d98e7d2b929dc1672f30a1644f7
Arvind Talekar <arvindtalekar6391@gmail.com>
2025-10-23 22:27:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+ import Nav from './Components/Nav'
+ import Heading from './Components/Heading'
+ import Cards from './Components/Cards'
+ import Shop from './Components/Shop'
+ import Footer from './Components/Footer'
+ const App = () => {
+   return (
+     <div className='h-265 w-full bg-gray-200 rounded-2xl'>
+<Nav />
+<Heading/>
+<Cards/>
+<Shop/>
+<Footer/>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
